a399adb94ec6f8b380836d4a3fca82f2fefae748
anvesh balaji <anvesh.b@globeshout.com>
2020-12-30 14:56:37 +0530

    Updates

---
+++ precache-manifest.a3d58a5e6ab39fee2d8cd11f23f3180e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1006fa86c36b1b0f4a9e03fe098f39",
+    "url": "/nrs-cms/index.html"
+  },
+  {
+    "revision": "0193f04ba9e7a5fa7f8b",
+    "url": "/nrs-cms/static/css/2.68dd0589.chunk.css"
+  },
+  {
+    "revision": "756a918ac86c12d6057d",
+    "url": "/nrs-cms/static/css/main.c968ba79.chunk.css"
+  },
+  {
+    "revision": "0193f04ba9e7a5fa7f8b",
+    "url": "/nrs-cms/static/js/2.8452270e.chunk.js"
+  },
+  {
+    "revision": "2ab61a961bdc0589b26baa3f9b6e381f",
+    "url": "/nrs-cms/static/js/2.8452270e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756a918ac86c12d6057d",
+    "url": "/nrs-cms/static/js/main.1a46213a.chunk.js"
+  },
+  {
+    "revision": "736344c17469328ccd8a",
+    "url": "/nrs-cms/static/js/runtime-main.79717ab9.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/nrs-cms/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/nrs-cms/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/nrs-cms/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/nrs-cms/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  }
+]);
